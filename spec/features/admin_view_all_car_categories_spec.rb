086b67a5e8b4e32259ddcfc803fa224dc585b5b7
Andre Cesar Gomes <andre270492@gmail.com>
2019-11-29 15:14:00 -0200

    fix typo

--- spec/features/admin_view_all_car_categories_spec.rb
+++ spec/features/admin_view_all_car_categories_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-feature 'Administrator view subsidiaries' do
+feature 'Administrator view all categories' do
   scenario 'successfully' do
-    CarCategory.create(name: 'Sedan', 
+    CarCategory.create(name: 'B', 
                       daily_rate: '70.00', 
                       car_insurance: '500.00',
                       third_party_insurance: '200.00')
-    CarCategory.create(name: 'Hatch', 
+    CarCategory.create(name: 'A', 
                       daily_rate: '62.00', 
                       car_insurance: '323.22',
                       third_party_insurance: '99.99')
@@ -15,11 +15,11 @@ feature 'Administrator view subsidiaries' do
     click_on 'Categorias'
     
 
-    expect(page).to have_content('Sedan')
+    expect(page).to have_content('B')
     expect(page).to have_content('70.00')
     expect(page).to have_content('500.00')
     expect(page).to have_content('200.00')
-    expect(page).to have_content('Hatch')
+    expect(page).to have_content('A')
     expect(page).to have_content('62.00')
     expect(page).to have_content('323.22')
     expect(page).to have_content('99.99')
