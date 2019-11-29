d92da9970297430253f5b70cf1e3faa172fda95f
Andre Cesar Gomes <andre270492@gmail.com>
2019-11-29 17:37:02 -0200

    add register manufacturer option

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -6,4 +6,24 @@ class ManufacturersController < ApplicationController
   def show
     @manufacturer = Manufacturer.find(params[:id])
   end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+  
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+    if @manufacturer.save
+      flash[:success] = "Manufacturer successfully created"
+      redirect_to @manufacturer
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  private
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name)
+  end
 end
